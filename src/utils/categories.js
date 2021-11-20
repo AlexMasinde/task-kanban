5bc7c74a8edcc9ddcb49144ba2264193ed974e41
Jane Njeri <daktari2033@gmail.com>
2021-11-20 03:39:56 +0300

    finalized asset preparation

---
+++ src/utils/categories.js
@@ -0,0 +1,18 @@
+export default categories = [
+  {
+    name: "iOS",
+    color: "000000",
+  },
+  {
+    name: "Web",
+    color: "4765B0",
+  },
+  {
+    name: "Android",
+    color: "17A14E",
+  },
+  {
+    name: "Desktop",
+    color: "CB9647",
+  },
+];
